5a8f0723f82fc221603c066a7ebd86f532137967
JXN <juandiegomartizcerpa@gmail.com>
2026-09-10 16:22:11 -0500

    Update evaluacion_bp.py
    reestructuración

--- Routes/evaluacion_bp.py
+++ Routes/evaluacion_bp.py
@@ -1,19 +1,20 @@
 from flask import Blueprint
 from Controllers.evaluacionController import evaluacionController
 
+evaluacion_bp = Blueprint('evaluacion_bp', __name__)
 
-evaluacion_bp = Blueprint("evaluacion_bp", __name__)
-
-
-@evaluacion_bp.route("/", methods=["GET"])
-def show():
+@evaluacion_bp.route('/', methods=['GET'])
+def home():
     return evaluacionController.show()
 
-
-@evaluacion_bp.route("/", methods=["POST"])
+@evaluacion_bp.route('/', methods=['POST'])
 def add():
     return evaluacionController.add()
 
-@evaluacion_bp.route("/", methods=["DELETE"])
-def delete():
-    return evaluacionController.add()
+@evaluacion_bp.route('/<string:eva_uuid>', methods=['PUT'])
+def update(eva_uuid):
+    return evaluacionController.update(eva_uuid)
+
+@evaluacion_bp.route('/<string:eva_uuid>', methods=['DELETE'])
+def delete(eva_uuid):
+    return evaluacionController.delete(eva_uuid)
