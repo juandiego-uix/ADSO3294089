17c3b3db5a71a02e8809f7b9d00a86d95fae1ff8
JXN <juandiegomartizcerpa@gmail.com>
2026-09-10 16:22:30 -0500

    Update imparte_bp.py
    reestructuración

--- Routes/imparte_bp.py
+++ Routes/imparte_bp.py
@@ -1,20 +1,20 @@
 from flask import Blueprint
 from Controllers.imparteController import imparteController
 
+imparte_bp = Blueprint('imparte_bp', __name__)
 
-imparte_bp = Blueprint("imparte_bp", __name__)
-
-
-@imparte_bp.route("/", methods=["GET"])
-def show():
+@imparte_bp.route('/', methods=['GET'])
+def home():
     return imparteController.show()
 
-
-@imparte_bp.route("/", methods=["POST"])
+@imparte_bp.route('/', methods=['POST'])
 def add():
     return imparteController.add()
 
+@imparte_bp.route('/<string:imp_uuid>', methods=['PUT'])
+def update(imp_uuid):
+    return imparteController.update(imp_uuid)
 
-@imparte_bp.route("/", methods=["DELETE"])
-def delete():
-    return imparteController.add()
+@imparte_bp.route('/<string:imp_uuid>', methods=['DELETE'])
+def delete(imp_uuid):
+    return imparteController.delete(imp_uuid)
