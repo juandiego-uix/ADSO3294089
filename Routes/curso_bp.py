1510858c65dbbfdb61f9a114d9776f13a74bc9e9
JXN <juandiegomartizcerpa@gmail.com>
2026-09-10 16:20:31 -0500

    Update curso_bp.py
    reestructuración

--- Routes/curso_bp.py
+++ Routes/curso_bp.py
@@ -1,19 +1,20 @@
 from flask import Blueprint
 from Controllers.cursoController import cursoController
 
+curso_bp = Blueprint('curso_bp', __name__)
 
-curso_bp = Blueprint("curso_bp", __name__)
-
-
-@curso_bp.route("/", methods=["GET"])
-def show():
+@curso_bp.route('/', methods=['GET'])
+def home():
     return cursoController.show()
 
-
-@curso_bp.route("/", methods=["POST"])
+@curso_bp.route('/', methods=['POST'])
 def add():
     return cursoController.add()
 
-@curso_bp.route("/", methods=["DELETE"])
-def delete():
-    return cursoController.add()
+@curso_bp.route('/<string:cur_uuid>', methods=['PUT'])
+def update(cur_uuid):
+    return cursoController.update(cur_uuid)
+
+@curso_bp.route('/<string:cur_uuid>', methods=['DELETE'])
+def delete(cur_uuid):
+    return cursoController.delete(cur_uuid)
