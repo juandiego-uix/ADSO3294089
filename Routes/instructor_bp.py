e47d01f23938da5b9c91d34628f1174edd3cb0b8
JXN <juandiegomartizcerpa@gmail.com>
2026-09-10 16:22:52 -0500

    Update instructor_bp.py
    reestructuración

--- Routes/instructor_bp.py
+++ Routes/instructor_bp.py
@@ -1,19 +1,20 @@
 from flask import Blueprint
 from Controllers.instructorController import instructorController
 
+instructor_bp = Blueprint('instructor_bp', __name__)
 
-instructor_bp = Blueprint("instructor_bp", __name__)
-
-
-@instructor_bp.route("/", methods=["GET"])
-def show():
+@instructor_bp.route('/', methods=['GET'])
+def home():
     return instructorController.show()
 
-
-@instructor_bp.route("/", methods=["POST"])
+@instructor_bp.route('/', methods=['POST'])
 def add():
     return instructorController.add()
 
-@instructor_bp.route("/", methods=["DELETE"])
-def delete():
-    return instructorController.add()
+@instructor_bp.route('/<string:ins_uuid>', methods=['PUT'])
+def update(ins_uuid):
+    return instructorController.update(ins_uuid)
+
+@instructor_bp.route('/<string:ins_uuid>', methods=['DELETE'])
+def delete(ins_uuid):
+    return instructorController.delete(ins_uuid)
