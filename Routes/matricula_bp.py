29d4225830b6a21ccd64b7060d476d453fb917fe
JXN <juandiegomartizcerpa@gmail.com>
2026-09-10 16:23:36 -0500

    Update matricula_bp.py
    reestructuración

--- Routes/matricula_bp.py
+++ Routes/matricula_bp.py
@@ -1,19 +1,20 @@
 from flask import Blueprint
 from Controllers.matriculaController import matriculaController
 
+matricula_bp = Blueprint('matricula_bp', __name__)
 
-matricula_bp = Blueprint("matricula_bp", __name__)
-
-
-@matricula_bp.route("/", methods=["GET"])
-def show():
+@matricula_bp.route('/', methods=['GET'])
+def home():
     return matriculaController.show()
 
-
-@matricula_bp.route("/", methods=["POST"])
+@matricula_bp.route('/', methods=['POST'])
 def add():
     return matriculaController.add()
 
-@matricula_bp.route("/", methods=["DELETE"])
-def delete():
-    return matriculaController.add()
+@matricula_bp.route('/<string:mat_uuid>', methods=['PUT'])
+def update(mat_uuid):
+    return matriculaController.update(mat_uuid)
+
+@matricula_bp.route('/<string:mat_uuid>', methods=['DELETE'])
+def delete(mat_uuid):
+    return matriculaController.delete(mat_uuid)
