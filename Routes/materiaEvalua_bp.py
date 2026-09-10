82b984c665ec022dbec0d2603ebbf265948891ba
JXN <juandiegomartizcerpa@gmail.com>
2026-09-10 16:23:14 -0500

    Update materiaEvalua_bp.py
    reestructuración

--- Routes/materiaEvalua_bp.py
+++ Routes/materiaEvalua_bp.py
@@ -1,19 +1,20 @@
 from flask import Blueprint
 from Controllers.materiaEvaluaController import materiaEvaluaController
 
+materia_evalua_bp = Blueprint('materia_evalua_bp', __name__)
 
-materia_evalua_bp = Blueprint("materia_evalua_bp", __name__)
-
-
-@materia_evalua_bp.route("/", methods=["GET"])
-def show():
+@materia_evalua_bp.route('/', methods=['GET'])
+def home():
     return materiaEvaluaController.show()
 
-
-@materia_evalua_bp.route("/", methods=["POST"])
+@materia_evalua_bp.route('/', methods=['POST'])
 def add():
     return materiaEvaluaController.add()
 
-@materia_evalua_bp.route("/", methods=["DELETE"])
-def delete():
-    return materiaEvaluaController.add()
+@materia_evalua_bp.route('/<string:mate_uuid>', methods=['PUT'])
+def update(mate_uuid):
+    return materiaEvaluaController.update(mate_uuid)
+
+@materia_evalua_bp.route('/<string:mate_uuid>', methods=['DELETE'])
+def delete(mate_uuid):
+    return materiaEvaluaController.delete(mate_uuid)
